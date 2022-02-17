f3c7139b81291b15641a3de085968a3d649752c6
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2022-02-16 22:25:17 -0800

    Created Project Files and pushed to netlify

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AdiCoin
+# Adi Coin Mining Site
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b5141d8b-f854-4f9b-afe3-917f1edeb2ee/deploy-status)](https://app.netlify.com/sites/adicoin/deploys)
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cubes from "./cube";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cubes />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
